1a2c9db6773c01ebc43a4eab4a9b8b2a716ded25
123 <chenjiuxiang1994@gmail.com>
2019-09-14 12:23:26 -0400

    new

---
+++ SportsStore/Vic.SportsStore/Vic.SportsStore.DebugConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vic.SportsStore.Domain.Concrete;
+using Vic.SportsStore.Domain.Entities;
+
+namespace Vic.SportsStore.DebugConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new EFDbContext())
+            {
+                var product = new Product()
+                {
+                    Name = "apple",
+                    Price = 1.2m,
+                    Description = "this is an apple",
+                    Category = "food",
+                };
+
+                ctx.Products.Add(product);
+                ctx.SaveChanges();
+            }
+
+            Console.WriteLine("done.");
+            Console.ReadLine();
+        }
+    }
+}
