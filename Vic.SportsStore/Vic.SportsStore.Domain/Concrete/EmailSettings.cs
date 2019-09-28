889eeda8dd92095d88c59b4a0c7b476bab724c69
123 <chenjiuxiang1994@gmail.com>
2019-09-28 18:05:28 -0400

    new

--- Vic.SportsStore/Vic.SportsStore.Domain/Concrete/EmailSettings.cs
+++ Vic.SportsStore/Vic.SportsStore.Domain/Concrete/EmailSettings.cs
@@ -6,5 +6,6 @@ using System.Threading.Tasks;
 
 namespace Vic.SportsStore.Domain.Concrete
 {
-    public class EmailSettings { public string MailToAddress = "orders@example.com"; public string MailFromAddress = "sportsstore@example.com"; public bool UseSsl = true; public string Username = "MySmtpUsername"; public string Password = "MySmtpPassword"; public string ServerName = "smtp.example.com"; public int ServerPort = 587; public bool WriteAsFile = true; public string FileLocation = @"d:\sports_store_emails"; }
+    public class EmailSettings { public string MailToAddress = "orders@example.com"; public string MailFromAddress = "sportsstore@example.com"; public bool UseSsl = true; public string Username = "MySmtpUsername"; public string Password = "MySmtpPassword"; public string ServerName = "smtp.example.com"; public int ServerPort = 587; public bool WriteAsFile = true; public string FileLocation = @"C:\Documents\2019fall\vic\git\sports_store_emails"; }
+    
 }
