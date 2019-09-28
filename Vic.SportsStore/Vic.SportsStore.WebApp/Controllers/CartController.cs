afbdc57009556eb83b71000a3f7d8e70da90692f
123 <chenjiuxiang1994@gmail.com>
2019-09-28 16:40:19 -0400

    add cart summary

--- Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/CartController.cs
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/CartController.cs
@@ -17,11 +17,6 @@ namespace Vic.SportsStore.WebApp.Controllers
 
         public RedirectToRouteResult AddToCart(Cart cart, int productId, string returnUrl) { Product product = repository.Products.FirstOrDefault(p => p.ProductId == productId); if (product != null) { cart.AddItem(product, 1); } return RedirectToAction("Index", new { returnUrl }); }
         public RedirectToRouteResult RemoveFromCart(Cart cart, int productId, string returnUrl) { Product product = repository.Products.FirstOrDefault(p => p.ProductId == productId); if (product != null) { cart.RemoveLine(product); } return RedirectToAction("Index", new { returnUrl }); }
-        private Cart GetCart()
-        {
-            Cart cart = (Cart)Session["Cart"];
-            if (cart == null) { cart = new Cart(); Session["Cart"] = cart; }
-            return cart;
-        }
+        public PartialViewResult Summary(Cart cart) { return PartialView(cart); }
     }
 }
