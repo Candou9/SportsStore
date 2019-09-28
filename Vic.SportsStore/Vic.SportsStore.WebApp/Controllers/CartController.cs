2dbcde55a27dd5f36d21fe0aa9d03af14a55589a
123 <chenjiuxiang1994@gmail.com>
2019-09-28 16:15:05 -0400

    new

--- Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/CartController.cs
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/CartController.cs
@@ -13,19 +13,10 @@ namespace Vic.SportsStore.WebApp.Controllers
     {
         private IProductsRepository repository; public CartController(IProductsRepository repo) { repository = repo; }
 
-        public ViewResult Index(string returnUrl) { return View(new CartIndexViewModel { Cart = GetCart(), ReturnUrl = returnUrl }); }
-        public RedirectToRouteResult AddToCart(int productId, string returnUrl)
-        {
-            Product product = repository.Products.FirstOrDefault(p => p.ProductId == productId);
-            if (product != null) { GetCart().AddItem(product, 1); }
-            return RedirectToAction("Index", new { returnUrl });
-        }
-        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
-        {
-            Product product = repository.Products.FirstOrDefault(p => p.ProductId == productId);
-            if (product != null) { GetCart().RemoveLine(product); }
-            return RedirectToAction("Index", new { returnUrl });
-        }
+        public ViewResult Index(Cart cart, string returnUrl) { return View(new CartIndexViewModel { ReturnUrl = returnUrl, Cart = cart }); }
+
+        public RedirectToRouteResult AddToCart(Cart cart, int productId, string returnUrl) { Product product = repository.Products.FirstOrDefault(p => p.ProductId == productId); if (product != null) { cart.AddItem(product, 1); } return RedirectToAction("Index", new { returnUrl }); }
+        public RedirectToRouteResult RemoveFromCart(Cart cart, int productId, string returnUrl) { Product product = repository.Products.FirstOrDefault(p => p.ProductId == productId); if (product != null) { cart.RemoveLine(product); } return RedirectToAction("Index", new { returnUrl }); }
         private Cart GetCart()
         {
             Cart cart = (Cart)Session["Cart"];
