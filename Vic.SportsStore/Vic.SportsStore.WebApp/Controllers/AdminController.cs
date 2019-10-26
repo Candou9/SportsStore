47640f0bb6d53f7268bac9bd8f513ec5288ec5f6
123 <chenjiuxiang1994@gmail.com>
2019-10-26 16:21:42 -0400

    new

---
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vic.SportsStore.Domain.Abstract;
+using Vic.SportsStore.Domain.Entities;
+
+namespace Vic.SportsStore.WebApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductsRepository repository;
+        public AdminController(IProductsRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+        public ViewResult Edit(int productId)
+        {
+            Product product = repository
+            .Products
+            .FirstOrDefault(p => p.ProductId == productId);
+            return View(product);
+        }
+    }
+}
